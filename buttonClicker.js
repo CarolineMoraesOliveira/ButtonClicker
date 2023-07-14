5e229d43c30f8b6f6c0ed73841472b9d8f5df2d6
Caroline M Oliveira <126394295+CarolineMoraesOliveira@users.noreply.github.com>
2023-07-14 15:53:39 -0300

    Add files via upload

---
+++ buttonClicker.js
@@ -0,0 +1,13 @@
+console.log('Running main.js');
+
+function ChangeName(element){
+if (element.innerHTML == "Login"){
+    (element.innerHTML = "Logout")
+}
+else {
+    (element.innerHTML = "Login")
+}
+}
+
+
+
